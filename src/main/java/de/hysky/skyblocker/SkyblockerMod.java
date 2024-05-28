de8d6d1fa84eec2d8b981e5e8270c03849669fb2
UpFault <johian2004@gmail.com>
2024-05-28 16:34:10 -0500

    Added EssenceShopPrice Feature, moved concatenate methods from ChocolateFactorySolver to Utils as they most likely will be used many times

--- src/main/java/de/hysky/skyblocker/SkyblockerMod.java
+++ src/main/java/de/hysky/skyblocker/SkyblockerMod.java
@@ -185,6 +185,7 @@ public class SkyblockerMod implements ClientModInitializer {
         EggFinder.init();
         TimeTowerReminder.init();
         SkyblockTime.init();
+        new EssenceShopPrice();
 
         Scheduler.INSTANCE.scheduleCyclic(Utils::update, 20);
         Scheduler.INSTANCE.scheduleCyclic(DiscordRPCManager::updateDataAndPresence, 200);
